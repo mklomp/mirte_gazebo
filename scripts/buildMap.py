c4e4aad79fcbcafdc45397d88bb5fe2096e34e57
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2023-08-17 12:22:36 +0200

    Cleanup and buildMap fix

--- scripts/buildMap.py
+++ scripts/buildMap.py
@@ -5,6 +5,16 @@ import os, sys
 import yaml
 
 
+def get_subdir_path(sub_dir):
+    # Get the directory this module is located in
+    abs_path_module = os.path.realpath(__file__)
+    module_dir, _ = os.path.split(abs_path_module)
+
+    dir_path = os.path.join(module_dir, sub_dir)
+
+    return dir_path
+
+
 def get_file_path(sub_dir, file_name, default_ext):
     """
     Get the absolute path of a resource file, which may be relative to
