5c87116323858ca8eebfe2d1e62e3549112831a8
Arend-Jan van Hilten (TU) <a.j.vanhilten@tudelft.nl>
2023-08-17 12:33:44 +0200

    Cleanup and rosdep python-yaml

--- scripts/xmlformat.sh
+++ scripts/xmlformat.sh
@@ -2,3 +2,4 @@
 find . -wholename "**/*.launch" -type f -exec xmllint --output '{}' --format '{}' \;
 find . -wholename "**/*.xacro" -type f -exec xmllint --output '{}' --format '{}' \;
 find . -wholename "**/*.world" -type f -exec xmllint --output '{}' --format '{}' \;
+find . -wholename "**/*.xml" -type f -exec xmllint --output '{}' --format '{}' \;
