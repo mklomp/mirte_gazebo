4ab48dba665ffcfc6766f798a8c7e89af755456f
Arend-Jan van Hilten <arendjan18@gmail.com>
2024-08-08 12:23:05 +0200

    fix conver ir mirte lab3

--- src/convert_ir.cpp
+++ src/convert_ir.cpp
@@ -1,65 +1,69 @@
-#include "std_msgs/String.h"
+#include "std_msgs/msg/string.hpp"
 #include <iostream>
-#include <mirte_msgs/GetIntensity.h>
-#include <mirte_msgs/GetIntensityDigital.h>
-#include <mirte_msgs/Intensity.h>
-#include <mirte_msgs/IntensityDigital.h>
+// #include <mirte_msgs/msg/
+#include <mirte_msgs/srv/get_intensity.hpp>
+#include <mirte_msgs/srv/get_intensity.hpp>
+#include <mirte_msgs/msg/intensity.hpp>
+#include <mirte_msgs/msg/intensity_digital.hpp>
+#include <mirte_msgs/srv/get_intensity_digital.hpp>
+// #include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <std_srvs/srv/empty.h>
+using std::placeholders::_1;
+using std::placeholders::_2;
 
-#include <ros/ros.h>
-#include <sensor_msgs/Image.h>
-#include <std_srvs/Empty.h>
-
-class map_ir {
+class map_ir : public rclcpp::Node {
 
 public:
-  map_ir(ros::NodeHandle nh, std::string side) {
+  map_ir(std::string  node_name, std::string side) : Node(node_name) {
 
-    pub_ = n_.advertise<mirte_msgs::Intensity>("/mirte/intensity/" + side, 10);
-    pub_dig_ = n_.advertise<mirte_msgs::IntensityDigital>(
+    pub_ = this->create_publisher<mirte_msgs::msg::Intensity>("/mirte/intensity/" + side, 10);
+    pub_dig_ = this->create_publisher<mirte_msgs::msg::IntensityDigital>(
         "/mirte/intensity/" + side + "_digital", 10);
-    sub_ = n_.subscribe("/mirte/camera_ir_" + side + "/image_raw", 10,
-                        &map_ir::callback, this);
-    server_ = n_.advertiseService("/mirte/get_intensity_" + side,
-                                  &map_ir::service_cb, this);
+    sub_ = this->create_subscription<sensor_msgs::msg::Image>("/camera_" + side + "/image_raw", 10,
+                        std::bind(&map_ir::callback, this, _1));
+    server_ = this->create_service<mirte_msgs::srv::GetIntensity>("/mirte/get_intensity_" + side,
+                                std::bind(  &map_ir::service_cb, this, _1, _2));
     server_dig_ =
-        n_.advertiseService("/mirte/get_intensity_" + side + "_digital",
-                            &map_ir::service_cb_dig, this);
+        this->create_service<mirte_msgs::srv::GetIntensityDigital>("/mirte/get_intensity_" + side + "_digital",
+                           std::bind( &map_ir::service_cb_dig, this, _1, _2));
   }
 
-  void callback(const sensor_msgs::Image &img) {
-    mirte_msgs::Intensity irInt;
-    irInt.value = img.data[0];
-    irInt.header = img.header;
+  void callback(const sensor_msgs::msg::Image::SharedPtr img) {
+    mirte_msgs::msg::Intensity irInt;
+    irInt.value = img->data[0];
+    irInt.header = img->header;
     this->lastIntensity = irInt.value;
-    pub_.publish(irInt);
+    pub_->publish(irInt);
 
-    mirte_msgs::IntensityDigital irIntDig;
-    irIntDig.value = img.data[0] > 128;
-    irIntDig.header = img.header;
+    mirte_msgs::msg::IntensityDigital irIntDig;
+    irIntDig.value = img->data[0] > 128;
+    irIntDig.header = img->header;
     this->lastIntensityDig = irIntDig.value;
-    pub_dig_.publish(irIntDig);
+    pub_dig_->publish(irIntDig);
   }
 
-  bool service_cb(mirte_msgs::GetIntensity::Request &req,
-                  mirte_msgs::GetIntensity::Response &res) {
+  bool service_cb(mirte_msgs::srv::GetIntensity::Request::SharedPtr req,
+                  mirte_msgs::srv::GetIntensity::Response::SharedPtr res) {
 
-    res.data = this->lastIntensity;
+    res->data = this->lastIntensity;
     return true;
   }
-  bool service_cb_dig(mirte_msgs::GetIntensityDigital::Request &req,
-                      mirte_msgs::GetIntensityDigital::Response &res) {
-    res.data = this->lastIntensityDig;
+  bool service_cb_dig(mirte_msgs::srv::GetIntensityDigital::Request::SharedPtr req,
+                      mirte_msgs::srv::GetIntensityDigital::Response::SharedPtr res) {
+    res->data = this->lastIntensityDig;
     return true;
   }
 
 private:
   // Create the necessary objects for subscribing and publishing
-  ros::NodeHandle n_;
-  ros::Publisher pub_;
-  ros::Publisher pub_dig_;
-  ros::Subscriber sub_;
-  ros::ServiceServer server_;
-  ros::ServiceServer server_dig_;
+  // rclcpp::Node n_;
+  rclcpp::Publisher<mirte_msgs::msg::Intensity>::SharedPtr pub_;
+  rclcpp::Publisher<mirte_msgs::msg::IntensityDigital>::SharedPtr pub_dig_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_;
+  rclcpp::Service<mirte_msgs::srv::GetIntensity>::SharedPtr server_;
+  rclcpp::Service<mirte_msgs::srv::GetIntensityDigital>::SharedPtr server_dig_;
 
   int lastIntensity = 0;
   bool lastIntensityDig = false;
@@ -68,12 +72,9 @@ private:
 int main(int argc, char **argv) {
   std::string side = argv[1];
   std::string name = "map_ir_" + side;
-  ros::init(argc, argv, name);
-  ros::NodeHandle nh;
+  rclcpp::init(argc, argv);
+   rclcpp::spin(std::make_shared<map_ir>(name, side));
+  rclcpp::shutdown();
 
-  // Create an object of class pcl_object_detector_sub_and_pub that will take
-  // care of everything
-  map_ir map_ir_object(nh, side);
-  ros::spin();
   return 0;
 }
